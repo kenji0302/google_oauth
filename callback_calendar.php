498427d0621d9769a86741fe12bc69891ece2a53
Kenji Ono <kenji0302@gmail.com>
2017-07-13 14:31:48 +0900

    カレンダーへのアクセス追加

--- callback_calendar.php
+++ callback_calendar.php
@@ -5,7 +5,7 @@
 
 $APP_ID = getenv('APP_ID');
 $APP_SECRET = getenv('APP_SECRET');
-$OAUTH_CALLBACK ='http://127.0.0.1/callback.php';
+$OAUTH_CALLBACK ='http://127.0.0.1/callback_calendar.php';
 
 if (empty($_GET['code'])){
     echo "error";
@@ -49,8 +49,23 @@ var_dump($access_token_response);
 var_dump($me_json);
 
 
-//ユーザー情報取得
-$me = file_get_contents("https://www.googleapis.com/oauth2/v1/userinfo?access_token={$access_token}");
+////カレンダー一覧取得
+//$me = file_get_contents("https://www.googleapis.com/calendar/v3/users/me/calendarList?access_token={$access_token}");
+//
+//if ($me === false) {
+//    echo "error";
+//    exit;
+//}
+//$me_json = json_decode($me);
+//var_dump($access_token_response);
+//var_dump($me_json);
+
+
+
+
+//カレンダー一覧取得
+$calendarId = "calendar_address_example@gmail.com";
+$me = file_get_contents("https://www.googleapis.com/calendar/v3/calendars/{$calendarId}/events?access_token={$access_token}");
 
 if ($me === false) {
     echo "error";
